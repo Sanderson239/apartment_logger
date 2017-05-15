f0435ccf2090de6ee643070aee644620f7c9ef9b
Stephen Anderson <sa3196a@student.american.edu>
2017-05-15 16:10:11 -0700

    added address object class

--- index.js
+++ index.js
@@ -8,3 +8,15 @@ class apartment = {
     this.price = price;
   }
 }
+
+class address = {
+  constructor(street, city, state, country, zip) {
+    this.street = street;
+    this.city = city;
+    this.state = state;
+    this.country = country;
+    this.zip = zip;
+  }
+}
+
+const myHouse = new apartment('house', 1111, 'Fantastic!')
