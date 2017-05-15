9884dcd379bfacf982bea41f84dd6af52d6ff908
Stephen Anderson <sa3196a@student.american.edu>
2017-05-15 14:26:09 -0700

    created appartment class

---
+++ index.js
@@ -0,0 +1,10 @@
+class apartment = {
+  constructor(name, address, description, landlord, size, price) {
+    this.name = name;
+    this.address = address;
+    this.description = description;
+    this.landlord = landlord;
+    this.size = size;
+    this.price = price;
+  }
+}
