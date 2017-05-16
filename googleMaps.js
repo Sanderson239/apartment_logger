0e6d67fa14d6fa8b65fb48c5d7820ae1ecc07e6c
Stephen Anderson <sa3196a@student.american.edu>
2017-05-16 16:51:36 -0700

    added createUrl function for creating a fetch url from apartment's address property. added fetchGpsCoordinates functino for returning gps coordinates from fetch url. added createMapMarker to make a google maps marker from gps coordinates

--- googleMaps.js
+++ googleMaps.js
@@ -5,3 +5,29 @@ function initMap() {
     zoom: 8
   });
 };
+
+var sampleUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address=Winnetka&key=AIzaSyAehpePt0KKDTY-K7nVNN2Rfs_ap20Bo-A';
+
+function createUrl(apartment) {
+  let aptAdd = apartment.address
+  let addressRegex = `${aptAdd.street.replace(' ', '+')},+${aptAdd.city.replace(' ', '+')},+{aptAdd.state}`
+  return `https://maps.googleapis.com/maps/api/geocode/json?address=${addressRegex}&key=AIzaSyAehpePt0KKDTY-K7nVNN2Rfs_ap20Bo-A`
+}
+
+function fetchGpsCoordinates(url) {
+fetch(url)
+  .then(url => {
+      return url.json()
+  })
+  .then(response => {
+    return response.results[0].geometry.location
+ })
+}
+
+function createMapMarker(coordinates, map) {
+  let marker = new google.maps.Marker({
+    position: coordinates,
+    map: map
+  })
+  return marker
+}
