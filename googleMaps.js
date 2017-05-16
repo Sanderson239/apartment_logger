eac9c638d874789c38e0c96290d039dcbdc8c044
Stephen Anderson <sa3196a@student.american.edu>
2017-05-16 14:57:09 -0700

    got google map to appear but only if the js code is directly in the html

--- googleMaps.js
+++ googleMaps.js
@@ -1,7 +1,7 @@
-// function initMap() {
-//   var map = new google.maps.Map(document.getElementById('map'), {
-//     enter: {lat: -34.397, lng: 150.644},
-//     scrollwheel: false,
-//     zoom: 8
-//   });
-// };
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    enter: {lat: -34.397, lng: 150.644},
+    scrollwheel: false,
+    zoom: 8
+  });
+};
